6d96f588dfe0315ea89dee88e0370c4188029908
zhengpengyu666 <184543956@qq.com>
2021-09-17 10:34:15 +0800

    '1'

---
+++ router/test.go
@@ -0,0 +1,5 @@
+package router
+
+func Hello() string {
+	return "sdsafs"
+}
---
+++ server.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/labstack/echo/v4"
+	"helin/router"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/aa", func(context echo.Context) error {
+		hello := router.Hello()
+		return context.String(http.StatusOK, hello)
+	})
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Static("/static", "public/test.html")
+	e.Logger.Fatal(e.Start(":1323"))
+}
