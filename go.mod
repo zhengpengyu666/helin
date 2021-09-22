d2c58527c596972da24b06fc7853c52e0dc84b9f
zhengpengyu666 <184543956@qq.com>
2021-09-22 15:00:22 +0800

    2

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module helin
 go 1.16
 
 require (
-	github.com/labstack/echo/v4 v4.5.0 // indirect
-	github.com/spf13/viper v1.8.1 // indirect
+	github.com/labstack/echo/v4 v4.5.0
+	github.com/spf13/viper v1.8.1
 )
--- routers/router.go
+++ routers/router.go
@@ -2,13 +2,19 @@ package routers
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	"helin/routers/test"
 )
 
 func InitRouters() *echo.Echo {
+
 	e := echo.New()
 	// routers
 	e.Static("/static", "static/test.html")
+	e.Use(middleware.GzipWithConfig(middleware.GzipConfig{
+		Level: 5,
+	}))
+	e.Static("/obj", "obj")
 	api := e.Group("/api")
 	{
 		api.POST("/", test.Hello)
